692e2a580431cb26ee10255570e580c740466ee8
Chris Wong <chriswong@Chriss-MacBook-Air.local>
2025-05-01 18:19:29 -0500

    added shape changing functionailities and size changing via volume

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let cirLocation = [400,300];
+let shape_location = [400,300];
 let velocity = [0,0];
 let acceleration = 2;
 let momentum = [0,0];
@@ -9,50 +9,51 @@ let left = false
 let right = false;
 let moving = false;
 let circles = [];
-//build a circle of circles for loop to create an array of circles
+let triangles = [];
+let squares = [];
+let currentColor = [0,0,0];
+let mode = 1;
+var mic;
+let vol = 0;
 
 function preload() {
-  img = loadImage('/assets/fractal.jpeg');
+  //img = loadImage('/assets/fractal.jpeg');
 }
 function setup() {
   createCanvas(800, 600);
+  mic = new p5.AudioIn();
+  mic.start();
 }
 function draw() {
   background(220);
-  //image(img, 0, 0, width, height);
-  circle(cirLocation[0],cirLocation[1],80);
-  circleMovement();
+  let vol = mic.getLevel();
+  modeHandling();
+  movement();
   boundaryControl();
-  text("circle x: " + cirLocation[0] + "  circle y: " + cirLocation[1],10,30)
+  dragMovement();
+  if (up === false && down === false && left === false && right === false){
+    moving = false;
+  }
+  text("circle x: " + shape_location[0] + "  circle y: " + shape_location[1],10,30)
   text("velocity: " + velocity ,10 , 50)
   text("origin",10,10)
   text("+x",700,10)
   text("+y",10,500)
   //text("momentum x: " + momentum[0] + "  momentum y: " + momentum[1],10,50)
-  grandExplosion();
-  dragMovement();
-  if (up === false && down === false && left === false && right === false){
-    moving = false;
-  }
 }
-
 function keyPressed() {
   console.log('Key pressed:', key);
   if (key === 'w' || key === 'ArrowUp'){
     up = true;
-    circle.push(cirLocation[0], cirLocation[1]);
   }
   if (key === 's' || key === 'ArrowDown'){
     down = true;
-    circle.push(cirLocation[0], cirLocation[1]);
   }
   if (key === 'a' || key === 'ArrowLeft'){
     left = true;
-    circle.push(cirLocation[0], cirLocation[1]);
   }
   if (key === 'd' || key === 'ArrowRight'){
     right = true;
-    circle.push(cirLocation[0], cirLocation[1]);
   }
   if (up === true || down === true || left === true || right === true){
     moving = true;
@@ -83,80 +84,111 @@ function dragMovement() {
     if (moving === false){ 
         console.log("dragging");
         if (velocity[0] > 0) {
-        cirLocation[0] += velocity[0]*0.1;
+        shape_location[0] += velocity[0]*0.1;
         velocity[0] -= drag;
         console.log("dragging right");
         }
         if (velocity[0] < 0) {
-        cirLocation[0] += velocity[0]*0.1;
+        shape_location[0] += velocity[0]*0.1;
         velocity[0] += drag;
         console.log("dragging left");
         }
         if (velocity[1] > 0) {
-        cirLocation[1] += velocity[1]*0.1;
+        shape_location[1] += velocity[1]*0.1;
         velocity[1] -= drag;
         console.log("dragging down");
         }
         if (velocity[1] < 0) {
-        cirLocation[1] += velocity[1]*0.1;
+        shape_location[1] += velocity[1]*0.1;
         velocity[1] += drag;
         console.log("dragging up");
         }
     }
 }
-
-function circleMovement() {
+function movement() {
   if (up === true){
-    cirLocation[1] += velocity[1]*0.1;
+    shape_location[1] += velocity[1]*0.1;
     console.log("moved up");
     velocity[1] -= acceleration;
   }
   if (down === true){
-    cirLocation[1] += velocity[1]*0.1;
+    shape_location[1] += velocity[1]*0.1;
     console.log("moved down");
     velocity[1] += acceleration;
   }
   if (left === true){
-    cirLocation[0] += velocity[0]*0.1;
+    shape_location[0] += velocity[0]*0.1;
     console.log("moved left");
     velocity[0] -= acceleration;
   }
   if (right === true){
-    cirLocation[0] += velocity[0]*0.1;
+    shape_location[0] += velocity[0]*0.1;
     console.log("moved right");
     velocity[0] += acceleration;
   }
 }
-
 function boundaryControl() {
-  if (cirLocation[0] < 40) {
-    cirLocation[0] = 50;
+  if (shape_location[0] < vol) {
+    shape_location[0] = vol;
     velocity[0] = -velocity[0];
-    fill(random(255), random(255), random(255));
+    currentColor = [random(255), random(255), random(255)];
+    fill(currentColor[0], currentColor[1], currentColor[2]);
   }
-  if (cirLocation[0] > width-40) {
-    cirLocation[0] = width-50;
+  if (shape_location[0] > width-vol) {
+    shape_location[0] = width-vol;
     velocity[0] = -velocity[0];
-    fill(random(255), random(255), random(255));
+    currentColor = [random(255), random(255), random(255)];
+    fill(currentColor[0], currentColor[1], currentColor[2]);
   }
-  if (cirLocation[1] < 40) {
-    cirLocation[1] = 50;
+  if (shape_location[1] < vol) {
+    shape_location[1] = vol;
     velocity[1] = -velocity[1];
-    fill(random(255), random(255), random(255));
+    currentColor = [random(255), random(255), random(255)];
+    fill(currentColor[0], currentColor[1], currentColor[2]);
   }
-  if (cirLocation[1] > height-40) {
-    cirLocation[1] = height-50;
+  if (shape_location[1] > height-vol) {
+    shape_location[1] = height-vol;
     velocity[1] = -velocity[1];
-    fill(random(255), random(255), random(255));
+    currentColor = [random(255), random(255), random(255)];
+    fill(currentColor[0], currentColor[1], currentColor[2]);
   }
 }
-function grandExplosion() {
-  if (velocity > 50) {
-    for (let i = 0; i < 100; i++) {
-      let x = random(0, width);
-      let y = random(0, height);
-      fill(random(255), random(255), random(255));
-      circle(x, y, 10);
+function modeHandling() {
+    if (mode === 1){ // circle mode
+        circles.push(createVector(shape_location[0], shape_location[1]));
     }
-  }
+    if (mode === 2){ // square mode
+        squares.push(createVector(shape_location[0], shape_location[1]));
+    }
+    if (mode === 3){ // triangle mode
+        triangles.push(createVector(shape_location[0], shape_location[1]));
+    }
+    for (let i = 0; i < circles.length; i++){
+        let pos = circles[i];
+        fill(currentColor[0], currentColor[1], currentColor[2]);
+        ellipse(pos.x, pos.y,(vol+1)*50);
+      }
+      for (let i = 0; i < squares.length; i++){
+        let pos = squares[i];
+        fill(currentColor[0], currentColor[1], currentColor[2]);
+        square(pos.x-40, pos.y-40,(vol+1)*50);
+      }
+      for (let i = 0; i < triangles.length; i++){
+        let pos = triangles[i];
+        fill(currentColor[0], currentColor[1], currentColor[2]);
+        triangle(pos.x, pos.y-((vol+0.5)*50), pos.x+((vol+0.5)*50), pos.y+((vol+0.5)*50), pos.x-((vol+0.5)*50), pos.y+((vol+0.5)*50));
+      }
+}
+function mouseClicked() {
+    if (mode < 3){
+        mode++;
+    }
+    else {
+        mode = 1;
+    }
+    console.log("mode: " + mode);
+}
+function micHandling() {
+    let vol = mic.getLevel();
+    console.log(vol);
 }
